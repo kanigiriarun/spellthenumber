d1df60fac2bf142d1b4e069c854d0e2b90280a7a
Arun Kumar Kanigiri <arun.kanigiri@sonata-software.com>
2020-05-28 10:25:12 +0530

    Converting number to words

---
+++ SpelltheNumber/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SpelltheNumber.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
